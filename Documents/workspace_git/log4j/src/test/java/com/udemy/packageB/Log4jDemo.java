5d3dc1e7ccaaa561ef09729e1036d8cf4a2d0650
DiyaR08 <DiyaRao08@gmail.com>
2019-05-07 14:15:23 -0700

    initial commit

---
+++ Documents/workspace_git/log4j/src/test/java/com/udemy/packageB/Log4jDemo.java
@@ -0,0 +1,23 @@
+package com.udemy.packageB;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4jDemo {
+	
+	//Create instance for logger class
+	private static Logger log = LogManager.getLogger(Log4jDemo.class);
+	
+	public static void main(String[] args) {
+	
+	// Log statements
+	log.info("Demo test");
+	log.error("Not generating the logs");
+	log.debug("Debugging");
+	log.fatal("Fatal error");
+		
+	}
+	
+	
+
+}
